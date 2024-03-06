dd415cf342af180786b623e9bc197c4e22bb5574
Emil Grużalski <emil.gruzalski@gmail.com>
2024-03-06 14:27:16 +0100

    Add content negotiation

--- Shared/DataTransferObjects/MaintenanceDto.cs
+++ Shared/DataTransferObjects/MaintenanceDto.cs
@@ -1,3 +1,4 @@
 ﻿namespace Shared.DataTransferObjects;
 
+[Serializable]
 public record MaintenanceDto(Guid Id, string Description, DateTime StartDate, DateTime EndDate, Decimal Cost);
--- Shared/DataTransferObjects/ReservationForCustomerDto.cs
+++ Shared/DataTransferObjects/ReservationForCustomerDto.cs
@@ -1,3 +1,4 @@
 ﻿namespace Shared.DataTransferObjects;
 
+[Serializable]
 public record ReservationForCustomerDto(Guid Id, DateTime StartDate, DateTime EndDate, string RoomNumber, string Status, Decimal TotalPrice);
