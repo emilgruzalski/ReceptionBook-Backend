b8098899a1b3547803bbda87ae73cf9e8dca4ccb
Emil Grużalski <emil.gruzalski@gmail.com>
2024-04-03 14:31:56 +0200

    Update UserForRegistrationDto.cs

--- Shared/DataTransferObjects/UserForRegistrationDto.cs
+++ Shared/DataTransferObjects/UserForRegistrationDto.cs
@@ -11,12 +11,10 @@ namespace Shared.DataTransferObjects
     {
         public string? FirstName { get; init; }
         public string? LastName { get; init; }
-        [Required(ErrorMessage = "Username is required")] 
-        public string? UserName { get; init; }
         [Required(ErrorMessage = "Password is required")] 
         public string? Password { get; init; }
+        [Required(ErrorMessage = "Email is required")] 
         public string? Email { get; init; }
-        public string? PhoneNumber { get; init; }
         //public ICollection<string>? Roles { get; init; }
     }
 }
